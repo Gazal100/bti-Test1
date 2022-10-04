e54af481f516b003db4e672b5dfda49e0d215947
Gazal100 <gazalgarg8065@gmail.com>
2022-10-04 16:08:49 -0400

    complete

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require("path");
+var test = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart(){
+    console.log("Express http server listening on " + HTTP_PORT);
+}
+
+test.get("/", function(req, res){
+    let resText = "<h2>Declaration</h2>";
+    resText += "<p>I declare that this test is my own work in accordance with Seneca Academic Policy. No part of this test has been copied manually or electronically from any other source.<br></p>";
+    resText += "Name: <mark><b>Gazal Garg</b></mark><br>"
+    resText += "<br>Student Number: <mark><b>107140212</b></mark><br>"
+    resText += "<br><a href = '/test1'> Go to Test1 </a> <br>"; 
+    res.send(resText);
+});
+
+
+test.get("/test1", function(req, res){
+    res.sendFile(path.join(__dirname, "/pages/test1.html"));
+});
+
+test.listen(HTTP_PORT, onHttpStart);
